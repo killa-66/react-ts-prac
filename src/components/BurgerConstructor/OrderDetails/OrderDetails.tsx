844b23ad5ecbd0ff6b658ee56504e5c2e66bfa33
killa-66 <killa669966@gmail.com>
2024-10-26 12:29:38 +0300

    modals

--- src/components/BurgerConstructor/OrderDetails/OrderDetails.tsx
+++ src/components/BurgerConstructor/OrderDetails/OrderDetails.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import styles from './OrderDetails.module.scss';
 import orderComplite from '../../../images/orderComplite.svg'
+import { orderTestData } from "../../../testData/testData";
+
 
 interface Props {
 
@@ -9,12 +11,11 @@ interface Props {
 const OrderDetails: FC<Props> = ({   }) => {
   return (
     <div className={styles.main}>
-      <p className="text text_type_digits-large">034536</p>
-      <p className="text text_type_main-medium">идентификатор заказа</p>
-      <img src={orderComplite} />
-      <p className="text text_type_main-default">Ваш заказ начали готовить</p>
-      <p className="text text_type_main-default">Дождитесь готовности на орбитальной станции</p>
-
+      <p className="text text_type_digits-large mb-8">{orderTestData.orderNumber}</p>
+      <p className="text text_type_main-medium mb-15">идентификатор заказа</p>
+      <img src={orderComplite} alt="Успешный заказ" className="mb-15" />
+      <p className="text text_type_main-default mb-2">{orderTestData.statusMessage}</p>
+      <p className={`${styles.textColor} text text_type_main-default mb-15`}>{orderTestData.waitingMessage}</p>
     </div>
   )
 }
---
+++ src/testData/testData.js
@@ -0,0 +1,5 @@
+export const orderTestData = {
+  orderNumber: "034536",
+  statusMessage: "Ваш заказ начали готовить",
+  waitingMessage: "Дождитесь готовности на орбитальной станции",
+};
