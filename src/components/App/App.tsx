24599caefc064379582915b75259513488331eea
killa-66 <killa669966@gmail.com>
2024-11-15 22:46:33 +0300

    stage 1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,32 @@
-import { FC, useState, useEffect } from 'react';
+import { FC } from 'react';
 import AppHeader from '../AppHeader/AppHeader';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import styles from './App.module.scss';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Login from '../Login/Login';
 
 const App: FC = () => {
 
   return (
     <div className={styles.page}>
       <AppHeader />
-      <main className={styles.menu}>
-        <>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </>
-      </main>
+      <Router>
+        <main className={styles.menu}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <BurgerIngredients />
+                  <BurgerConstructor />
+                </>
+              }
+            />
+            <Route path='/login' element={<Login />}/>
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 };
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,49 @@
+import { FC, SetStateAction, useState } from "react";
+import styless from './Login.module.scss';
+import { Button, EmailInput, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
+
+const Login:FC = () => {
+
+  const [value, setValue] = useState('bob@example.com')
+  const onChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setValue(e.target.value)
+  }
+
+  return (
+    <div className={styless.login}>
+      <h2 className="mb-6 text text_type_main-default">Вход</h2>
+      <EmailInput
+        onChange={onChange}
+        value={value}
+        name={'email'}
+        isIcon={false}
+        extraClass="mb-6"
+      />
+
+      <PasswordInput
+        onChange={onChange}
+        value={value}
+        name={'password'}
+        extraClass="mb-6"
+      />
+
+      <Button
+        htmlType={"button"}
+        extraClass="mb-20"
+        size="medium"
+      >Войти</Button>
+
+      <div>
+        <span className="text text_type_main-small">Вы - новый пользователь?&nbsp;</span>
+        <a href="/" className={`text text_type_main-small ${styless.text_link}`}>Зарегестрироваться</a>
+      </div>
+
+      <div>
+        <span className="text text_type_main-small">Забыли пароль?&nbsp;</span>
+        <a href="/" className={`text text_type_main-small ${styless.text_link}`}>Восстановить пароль</a>
+      </div>
+    </div>
+  )
+}
+
+export default Login
